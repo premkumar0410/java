66f863f4e47830efa6804f8c43b621bcab82e31b
premkumar0410 <rprem1042004@gmail.com>
2025-06-10 10:01:41 +0530

    added constructor and run method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,7 @@
 package org.example;
 public class Main{
     public static void main(String[] args) {
-        car car = new car();
-        car.setDoors(4);
-        car.setEngine("V12 engine");
-        car.setDrive("Is Driving");
-        System.out.println(car.getDoors());
-        System.out.println(car.getDrive());
-        System.out.println(car.getEngine());
+        car car = new car(10,4,"on","Driving");
+        car.run();
     }
 }
--- src/main/java/org/example/car.java
+++ src/main/java/org/example/car.java
@@ -1,35 +1,48 @@
 package org.example;
 
 public class car {
+    public int speed;
     private int doors;
-    public void setDoors(int doors){
+    private String engine;
+    private String drive;
+
+    public car(int speed ,int doors,String engine , String drive){
+        this.speed = speed;
         this.doors = doors;
+        this.engine = engine;
+        this.drive = drive;
     }
 
-    public int getDoors(){
+    public int getDoors() {
         return doors;
     }
 
-
-    private String engine;
-    public void setEngine(String engine){
-        this.engine = engine;
+    public void setDoors(int doors) {
+        this.doors = doors;
     }
-    public String getEngine(){
+
+    public String getEngine() {
         return engine;
     }
 
-
-
-    private String drive;
-    public void setDrive(String drive){
-        this.drive = drive;
+    public void setEngine(String engine) {
+        this.engine = engine;
     }
-    public String getDrive(){
+
+    public String getDrive() {
         return drive;
     }
 
-    public int speed;
+    public void setDrive(String drive) {
+        this.drive = drive;
+    }
 
+    public void run(){
+        if(speed > 0 && drive.equals("Driving") && engine.equals("on")&&doors == 4){
+            System.out.println("car is running");
+        }else{
+            System.out.println("car is not running");
+        }
+    }
 
 }
