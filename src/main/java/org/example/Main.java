d8003002513e3624e9a50030e2646e887210d0af
premkumar0410 <rprem1042004@gmail.com>
2025-06-11 12:33:34 +0530

    parametrized composition

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.example.laptop.components.processor;
+import org.example.laptop.components.graphic_card;
 import org.example.laptop.laptop;
 public class Main{
     public static void main(String[] args) {
-        laptop laptop = new laptop();
+        processor p1 = new processor();
+        graphic_card g1 = new graphic_card();
 
-        System.out.println(laptop.getGraphic_card().getBrand());
+        laptop laptop = new laptop("15.4", p1, 8,g1);
+
+        System.out.println(laptop);
+
+
     }
 }
