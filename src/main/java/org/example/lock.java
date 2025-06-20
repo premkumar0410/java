09ea82648938825f5a026ea9640a123ed2a7cb10
premkumar0410 <rprem1042004@gmail.com>
2025-06-20 18:32:37 +0530

    anonymous object

---
+++ src/main/java/org/example/lock.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class lock {
+    String code = "12345";
+
+    public String getCode() {
+        return code;
+    }
+}
--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -4,11 +4,10 @@ package org.example;
 
 public class main {
     public static void main(String[] args) {
-        door door = new door();
-        if(door.islocked("qwerty")){
-            System.out.println("door is locked");
-        }else{
-            System.out.println("door is unlocked");
+        if (new lock().getCode() == "12345") {
+            System.out.println("open");
+        } else {
+            System.out.println("closed");
         }
     }
 }
